0480108f4daf19fbca787df28858d7f453746c75
Ricardo <rickfat@proton.me>
2026-07-13 20:09:32 -0300

    feat: add salary percentage calculator exercise

---
+++ src/TaxCalculator.java
@@ -0,0 +1,15 @@
+/*
+Create a class that calculates a given percentage of a salary.
+* Create a class
+* Accept a salary value as input
+* Define the percentage value
+* Calculate the percentage
+ */
+public class TaxCalculator {
+    static void main(String[] args) {
+        float salary = 1500F;
+        float percentage = 0.1F;
+        float calculatedPercentage = salary * percentage;
+        System.out.println("The calculated percentage of the salary is: " + calculatedPercentage);
+    }
+}
