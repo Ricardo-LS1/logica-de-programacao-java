228cef19768759dc7cb312c1c238072e9341e1b8
Ricardo <rickfat@proton.me>
2026-07-13 18:15:30 -0300

    docs: add Java comment examples

--- VariablesAndPrimitiveTypes.java
+++ VariablesAndPrimitiveTypes.java
@@ -1,3 +1,11 @@
+// Everithing inside these comments will be ignored by the compiler
+
+/*
+aaaa
+ssss
+dddd
+*/
+
 public class VariablesAndPrimitiveTypes {
     public static void main(String[] args) {
         int age = 35;
