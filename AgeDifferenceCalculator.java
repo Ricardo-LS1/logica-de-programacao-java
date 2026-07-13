21eeab60d60c4012999742ce7290a80158adf216
Ricardo <rickfat@proton.me>
2026-07-13 18:46:00 -0300

    feat: add age difference calculator exercise

---
+++ AgeDifferenceCalculator.java
@@ -0,0 +1,16 @@
+/*
+Create a class named AgeDifferenceCalculator
+Create a variable to store the first age
+Create a variable to store the second age
+Create a variable to store the difference between age 1 and age 2
+Print the difference to the console
+*/
+public class AgeDifferenceCalculator {
+    public static void main(String[] args) {
+        int age1 = 50;
+	int age2 = 20;
+	int ageDifference = age1 - age2;
+
+	System.out.println("The difference between the age 1 and age 2 is: " + ageDifference);
+	}
+}
