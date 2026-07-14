634690ad66de7428d26ad4f1067f68a7f54ec202
Ricardo <rickfat@proton.me>
2026-07-14 14:47:55 -0300

    feat: add if-else conditional exercise

---
+++ src/ConditionalStatementsLesson01.java
@@ -0,0 +1,11 @@
+public class ConditionalStatementsLesson01 {
+    public static void main(String[] args) {
+        boolean condition = false;
+        if (condition) {
+            System.out.println("Inside the if block");
+        } else {
+            System.out.println("Inside the else block");
+        }
+        System.out.println("Outside the if-else statement");
+    }
+}
