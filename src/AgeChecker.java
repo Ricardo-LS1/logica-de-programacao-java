7596d843f889b1b2cf86ab6aaf7f132630f5e433
Ricardo <rickfat@proton.me>
2026-07-14 21:39:01 -0300

    feat: add age checker exercise

---
+++ src/AgeChecker.java
@@ -0,0 +1,17 @@
+/*
+Receive an age as input.
+
+If the age is greater than 18, print "Adult".
+Otherwise, print "Not an adult yet".
+ */
+public class AgeChecker {
+    static void main(String[] args) {
+        int age = 17;
+        // ==, !=, >, >=, <, <=
+        if (age > 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Not an adult yet");
+        }
+    }
+}
