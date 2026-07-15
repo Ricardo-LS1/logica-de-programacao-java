7f2d32f21de36fa35a2a6f62252637ef494e44a5
Ricardo <rickfat@proton.me>
2026-07-14 22:23:49 -0300

    feat: add tax rate output to conditional calculator

---
+++ src/ConditionalTaxCalculator.java
@@ -0,0 +1,24 @@
+/*
+Given a salary value.
+
+If the salary is greater than 4500, print 30% of its value.
+Otherwise, print 15% of its value.
+
+Challenge: Use only one variable to store the result and print it at the end.
+Challenge2: Indicate in the output whether it is 30% or 15%.
+ */
+public class ConditionalTaxCalculator {
+    static void main(String[] args) {
+        float salary = 1000.50F;
+        float result = 0F;
+        String percentage = "";
+        if (salary > 4500) {
+            result = salary * 0.3F;
+            percentage = "30%";
+        } else {
+            result = salary * 0.15F;
+            percentage = "15%";
+        }
+        System.out.println("The tax rate is " + percentage + ", and the tax amount is : " + result);
+    }
+}
