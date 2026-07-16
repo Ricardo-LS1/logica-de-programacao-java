f42cb3d565a688463dbc72a3a7c0f58c4ee83d5c
Ricardo <rickfat@proton.me>
2026-07-16 18:18:22 -0300

    feat: add swimming tournament category exercise

---
+++ src/SwimmingTournament.java
@@ -0,0 +1,33 @@
+/*
+As a user, I would like to have the names and ages of the participants in a swimming
+tournament, and I would like the system to display the following:
+
+Under 10 years old: <Name> will participate in the Children’s category.
+Between 11 and 15 years old: <Name> will participate in the Junior category.
+Between 16 and 19 years old: <Name> will participate in the Pre-Adult category.
+Over 20 years old: <Name> will participate in the Adult category.
+
+So that I can quickly classify all participants.
+
+Additional reading:
+Software development processes
+Agile methodologies
+Scrum
+ */
+public class SwimmingTournament {
+    static void main(String[] args) {
+        String name = "Goku";
+        int age = 30;
+        String category;
+        if (age < 10) {
+            category = "Children's";
+        } else if (age <= 15) {
+            category = "Junior";
+        } else if (age <= 19) {
+            category = "Pre-Adult";
+        } else {
+            category = "Adult";
+        }
+        System.out.println(name + " will participate in the " + category + " category");
+    }
+}
