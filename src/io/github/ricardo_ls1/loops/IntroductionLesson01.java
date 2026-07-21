ab8b1e4d150aca9902f455ab79d65e521a2e75f3
Ricardo <rickfat@proton.me>
2026-07-21 14:21:10 -0300

    feat: add while loop counter exercise

---
+++ src/io/github/ricardo_ls1/loops/IntroductionLesson01.java
@@ -0,0 +1,12 @@
+package io.github.ricardo_ls1.loops;
+
+public class IntroductionLesson01 {
+    static void main(String[] args) {
+        int counter = 0;
+
+        while (counter <= 10) {
+            System.out.println("Counter " + counter);
+            counter = counter + 1;
+        }
+    }
+}
