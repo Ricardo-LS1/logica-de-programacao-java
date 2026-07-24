dd56576d47f4b73e4318edfacb77d442a6ebd107
Ricardo <rickfat@proton.me>
2026-07-24 14:58:07 -0300

    feat: add for loop introduction exercise

---
+++ src/io/github/ricardo_ls1/loops/fors/IntroductionLesson01.java
@@ -0,0 +1,9 @@
+package io.github.ricardo_ls1.loops.fors;
+
+public class IntroductionLesson01 {
+    static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
