11919e61301b4d9d6558c83c9dede1dd285deff4
Ricardo <rickfat@proton.me>
2026-07-25 14:10:08 -0300

    feat: add multiplication table for loop exercise

---
+++ src/io/github/ricardo_ls1/loops/fors/Exercise02Lesson03.java
@@ -0,0 +1,10 @@
+package io.github.ricardo_ls1.loops.fors;
+
+public class Exercise02Lesson03 {
+    static void main(String[] args) {
+        int value = 5;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(value + "x" + i + " = " + value * i);
+        }
+    }
+}
