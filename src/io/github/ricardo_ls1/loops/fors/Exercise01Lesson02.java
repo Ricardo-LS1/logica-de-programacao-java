c49a9a05ef2774b7647d090578737b270c6b5d13
Ricardo <rickfat@proton.me>
2026-07-25 13:48:33 -0300

    feat: add integer squares for loop exercise

---
+++ src/io/github/ricardo_ls1/loops/fors/Exercise01Lesson02.java
@@ -0,0 +1,13 @@
+package io.github.ricardo_ls1.loops.fors;
+
+/*
+Create a program that displays the squares of all integers from 15 to 200.
+ */
+
+public class Exercise01Lesson02 {
+    static void main(String[] args) {
+        for (int i = 15; i <= 200; i++) {
+            System.out.println("The square of " + i + " is " + i * i + ".");
+        }
+    }
+}
